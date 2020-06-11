30fa0fa03ea107026f5c8c1084c6acfdbd5207c7
mobilego <anbudansri@gmail.com>
2020-06-11 12:18:04 +0530

    Adding custom Queue and worker

---
+++ rq_exporter/custom.py
@@ -0,0 +1,21 @@
+"""
+Custom RQ Classes.
+"""
+
+from rq import Worker, Queue
+from rq.job import Job
+
+
+class CustomJob(Job):
+    redis_job_namespace_prefix = 'rq:custom:job:'
+
+
+class CustomQueue(Queue):
+    redis_queue_namespace_prefix = 'rq:loki:'
+    job_class = CustomJob
+
+
+class CustomWorker(Worker):
+    redis_worker_namespace_prefix = 'rq:loki:worker:'
+    queue_class = CustomQueue
+    job_class = CustomJob
